9fb16a6d4b82f10ea8b0ed65c25b969949684050
RedAlexDad <boss6852@gmail.com>
2023-09-21 11:33:46 +0300

    Небольшая поправка
    Исправлен c БД с соответствием ПО и ER диаграммы

--- bmstu_lab/admin.py
+++ bmstu_lab/admin.py
@@ -1,11 +1,10 @@
 from django.contrib import admin
-from .models import MarsStation, EmployeeSpaceAgency, EmployeeOrganization, Location, GeographicalObject, Status, Users, Transport
+from .models import MarsStation, Scientist, Location, GeographicalObject, Status, Users, Transport
 
+admin.site.register(Users)
+admin.site.register(Scientist)
+admin.site.register(Transport)
 admin.site.register(MarsStation)
-admin.site.register(EmployeeSpaceAgency)
-admin.site.register(EmployeeOrganization)
+admin.site.register(Status)
 admin.site.register(Location)
 admin.site.register(GeographicalObject)
-admin.site.register(Status)
-admin.site.register(Users)
-admin.site.register(Transport)
